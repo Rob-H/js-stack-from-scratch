a16c4288466a55b9b96725facc51a97fc79e2581
Rob Haley <rob_haley@live.co.uk>
2017-04-12 09:08:51 +0100

    fix socket io

--- src/server/index.js
+++ src/server/index.js
@@ -22,8 +22,8 @@ app.use(STATIC_PATH, express.static('public'))
 
 routing(app)
 
-app.listen(WEB_PORT, () => {
+http.listen(WEB_PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running on port ${WEB_PORT} ${isProd ? '(production)' :
-           '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
+    '(development).\nKeep "yarn dev:wds" running in an other terminal'}.`)
 })
