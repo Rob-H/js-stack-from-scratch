9959f3e25fab90385a980d45d3bf043f73b3a040
Rob Haley <rob_haley@live.co.uk>
2017-04-18 13:45:02 +0100

    add bootstrap styling

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -2,6 +2,8 @@
 
 import 'babel-polyfill'
 
+import $ from 'jquery'
+import Tether from 'tether'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
@@ -16,6 +18,10 @@ import helloReducer from '../shared/reducer/hello'
 import { APP_CONTAINER_SELECTOR } from '../shared/config'
 import { isProd } from '../shared/util'
 import setupSocket from './socket'
+
+window.jQuery = $
+window.Tether = Tether
+require('bootstrap')
 
 /* eslint-disable no-underscore-dangle */
 const composeEnhancers = (isProd ? null : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
